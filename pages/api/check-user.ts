359107d7215a5787a3381152e7ce7b8eccd6e82f
Estifanos <estifanosbeyene21@gmail.com>
2022-12-06 17:19:33 -0500

    feat: add basic route to check user

---
+++ pages/api/check-user.ts
@@ -0,0 +1,23 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { authOptions } from "pages/api/auth/[...nextauth]";
+import { unstable_getServerSession } from "next-auth";
+
+type TReq = {
+  user: string;
+};
+
+const handler = async (req: NextApiRequest, res: NextApiResponse<TReq>) => {
+  const session = await unstable_getServerSession(req, res, authOptions);
+
+  if (!session) {
+    res.status(200).json({
+      user: "There is no user",
+    });
+  } else {
+    res.status(200).json({
+      user: "There is a user",
+    });
+  }
+};
+
+export default handler;
