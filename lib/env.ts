c7de36d3db7433514c9d1afc9f56312f0db978e4
Estifanos <estifanosbeyene21@gmail.com>
2022-12-06 18:14:26 -0500

    feat: setup email provider (not complete)

--- lib/env.ts
+++ lib/env.ts
@@ -1,8 +1,13 @@
 import { object, string } from "zod";
 
 const envSchema = object({
+  // facebook provider
   FACEBOOK_ID: string(),
   FACEBOOK_SECRET: string(),
+
+  // email provider
+  EMAIL_SERVER: string(),
+  EMAIL_FROM: string(),
 });
 
 const env = envSchema.safeParse(process.env);
--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,5 +1,6 @@
 import NextAuth, { NextAuthOptions } from "next-auth";
 import FBProvider from "next-auth/providers/facebook";
+import EmailProvider from "next-auth/providers/email";
 import env from "lib/env";
 
 export const authOptions: NextAuthOptions = {
@@ -7,6 +8,10 @@ export const authOptions: NextAuthOptions = {
     FBProvider({
       clientId: env.FACEBOOK_ID,
       clientSecret: env.FACEBOOK_SECRET,
+    }),
+    EmailProvider({
+      server: process.env.EMAIL_SERVER,
+      from: process.env.EMAIL_FROM,
     }),
   ],
 };
