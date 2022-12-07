ac4b16b69dd9be6ac0c99ae78708dc38ae07abff
Estifanos <estifanosbeyene21@gmail.com>
2022-12-07 05:06:46 -0500

    feat: set up email provider with gmail

--- lib/env.ts
+++ lib/env.ts
@@ -6,7 +6,10 @@ const envSchema = object({
   FACEBOOK_SECRET: string(),
 
   // email provider
-  EMAIL_SERVER: string(),
+  EMAIL_SERVER_USER: string(),
+  EMAIL_SERVER_PASSWORD: string(),
+  EMAIL_SERVER_HOST: string(),
+  EMAIL_SERVER_PORT: string(),
   EMAIL_FROM: string(),
 });
 
--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -9,8 +9,16 @@ export const authOptions: NextAuthOptions = {
       clientId: env.FACEBOOK_ID,
       clientSecret: env.FACEBOOK_SECRET,
     }),
+    // email provider
     EmailProvider({
-      server: process.env.EMAIL_SERVER,
+      server: {
+        host: env.EMAIL_SERVER_HOST,
+        port: env.EMAIL_SERVER_PORT,
+        auth: {
+          user: env.EMAIL_SERVER_USER,
+          pass: env.EMAIL_SERVER_PASSWORD,
+        },
+      },
       from: process.env.EMAIL_FROM,
     }),
   ],
