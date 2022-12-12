d887edd9a8e5678bf54c5ad4cde020e48b1e13b3
Estifanos <estifanosbeyene21@gmail.com>
2022-12-12 13:23:04 -0500

    feat(api/users):return all users

--- pages/api/users.ts
+++ pages/api/users.ts
@@ -1,14 +1,39 @@
 import type { NextApiRequest, NextApiResponse } from "next";
+import prisma from "@/lib/prisma";
+import { Prisma } from "@prisma/client";
 
-type User = {
-  id: number;
-  name?: string;
+const getRes = Prisma.validator<Prisma.UserArgs>()({
+  select: {
+    id: true,
+    profile: {
+      select: {
+        name: true,
+        image: true,
+      },
+    },
+  },
+});
+
+export type TGetRes = Prisma.UserGetPayload<typeof getRes>;
+
+/**
+ * @description  api handler to get all users in website
+ */
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method === "GET") {
+    const users: TGetRes[] = await prisma.user.findMany({
+      select: {
+        id: true,
+        profile: {
+          select: {
+            name: true,
+            image: true,
+          },
+        },
+      },
+    });
+    res.status(200).json(users);
+  }
 };
 
-// Fake users data
-const users: User[] = [{ id: 1 }, { id: 2 }, { id: 3 }];
-
-export default function handler(_req: NextApiRequest, res: NextApiResponse) {
-  // Get data from your database
-  res.status(200).json(users);
-}
+export default handler;
