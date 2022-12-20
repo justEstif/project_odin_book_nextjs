a5a94e5e12bca460eb55a55b1cf835b436c06cfa
Estifanos <estifanosbeyene21@gmail.com>
2022-12-20 10:11:51 -0500

    feat: add like post on POST req

--- pages/api/posts/[postId]/likes.ts
+++ pages/api/posts/[postId]/likes.ts
@@ -5,31 +5,44 @@ import type { NextApiHandler } from "next";
 const handler: NextApiHandler<TResponse> = async (req, res) => {
   const {
     method,
-    query: { postId },
+    query: { postId, currentUserId },
   } = req;
 
   switch (method) {
     /**
-     * @description get comments of a post
+     * @description get likes of a post
      * @access any logged in user
      */
     case "GET":
       if (typeof postId === "string") {
-        const data = await getPostLikes(postId);
+        const data = await getPostLikes({ postId });
         res.status(200).json(data);
       }
       res.status(403).end();
       break;
+    /**
+     * @description like or unlike a post
+     * @access any logged in user
+     */
+    case "POST":
+      if (typeof postId === "string" && typeof currentUserId === "string") {
+        const data = await createLike({ postId, currentUserId });
+        res.status(201).json(data);
+      }
+      res.status(403).end();
+      break;
     default:
-      res.setHeader("Allow", ["GET"]);
+      res.setHeader("Allow", ["GET", "PUT"]);
       res.status(405).end(`Method ${method} Not Allowed`);
   }
 };
 
 export default withAuth(handler);
-export type TResponse = Awaited<ReturnType<typeof getPostLikes>>;
+export type TResponse =
+  | Awaited<ReturnType<typeof getPostLikes>>
+  | Awaited<ReturnType<typeof createLike>>;
 
-const getPostLikes = async (postId: string) => {
+const getPostLikes = async ({ postId }: { postId: string }) => {
   return await prisma.post.findUnique({
     where: { id: postId },
     select: {
@@ -37,14 +50,42 @@ const getPostLikes = async (postId: string) => {
       likes: {
         select: {
           id: true,
-          profile: {
-            select: {
-              name: true,
-              image: true,
-            },
-          },
+          profile: { select: { name: true, image: true } },
+          // TODO: add friend status?
         },
       },
     },
   });
+};
+
+const createLike = async ({
+  postId,
+  currentUserId,
+}: {
+  postId: string;
+  currentUserId: string;
+}) => {
+  const like = await prisma.post.findUnique({
+    where: { id: postId },
+    select: {
+      likes: {
+        where: { id: currentUserId },
+        select: { id: true },
+      },
+    },
+  });
+
+  if (like) {
+    if (like.likes.some((el) => el.id === currentUserId)) {
+      return await prisma.post.update({
+        where: { id: postId },
+        data: { likes: { disconnect: { id: currentUserId } } },
+      });
+    } else {
+      return await prisma.post.update({
+        where: { id: postId },
+        data: { likes: { connect: { id: currentUserId } } },
+      });
+    }
+  }
 };
