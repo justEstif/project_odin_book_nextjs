cc7b9b270612b215dd5f727c516eeec8106523a1
Estifanos <estifanosbeyene21@gmail.com>
2022-12-20 06:24:09 -0500

    feat(app/posts): add posts page for testing

---
+++ app/posts/(components)/PostForm.tsx
@@ -0,0 +1,46 @@
+"use client";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { postSchema, TPostSchema } from "@/lib-server/validations/post";
+
+type Props = {};
+
+const PostForm = ({ }: Props) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<TPostSchema>({
+    resolver: zodResolver(postSchema),
+  });
+
+  const onSubmit: SubmitHandler<TPostSchema> = async (data) => {
+    try {
+      const res = await fetch("/api/posts", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+      if (res.ok) console.log("Created post");
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <label>
+          Content
+          <input {...register("content")} />
+          {errors.content?.message && <p>{errors.content?.message}</p>}
+        </label>
+        <button type="submit">Post</button>
+      </form>
+    </div>
+  );
+};
+
+export default PostForm;
---
+++ app/posts/page.tsx
@@ -0,0 +1,14 @@
+import PostForm from "./(components)/PostForm";
+
+type Props = {};
+
+const Page = ({}: Props) => {
+  return (
+    <div>
+      <div>Create post</div>
+      <PostForm />
+    </div>
+  );
+};
+
+export default Page;
