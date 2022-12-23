94c5b658f101295bb59a575d24c7418381b66de7
Estifanos <estifanosbeyene21@gmail.com>
2022-12-23 14:01:17 -0500

    feat: add request handler to accept friend req

--- pages/api/users/[userId]/received-requests.ts
+++ pages/api/users/[userId]/received-requests.ts
@@ -1,8 +1,11 @@
+import type { NextApiHandler } from "next";
 import withAuth from "@/lib-server/middleware/with-auth";
 import prisma from "@/lib-server/prisma";
-import type { NextApiHandler } from "next";
 
-const handler: NextApiHandler<TGetResponse> = async (req, res) => {
+const handler: NextApiHandler<TGetResponse | TPostResponse> = async (
+  req,
+  res
+) => {
   const {
     method,
     query: { userId, currentUserId },
@@ -22,11 +25,21 @@ const handler: NextApiHandler<TGetResponse> = async (req, res) => {
         const data = await getReceivedRequests(userId);
         res.status(200).json(data);
       }
-
+      res.status(403).end();
+      break;
+    /**
+     * @description accept friend request by disconnecting request and adding user to friends
+     * @access any logged in user
+     */
+    case "POST":
+      if (typeof userId === "string" && typeof currentUserId === "string") {
+        const data = await acceptFriendRequest({ currentUserId, userId });
+        res.status(200).json(data);
+      }
       res.status(403).end();
       break;
     default:
-      res.setHeader("Allow", ["GET"]);
+      res.setHeader("Allow", ["GET", "POST"]);
       res.status(405).end(`Method ${method} Not Allowed`);
   }
 };
@@ -38,5 +51,22 @@ const getReceivedRequests = async (id: string) => {
   return await prisma.user.findUnique({
     where: { id },
     select: { receivedRequests: true },
+  });
+};
+
+export type TPostResponse = Awaited<ReturnType<typeof acceptFriendRequest>>;
+const acceptFriendRequest = async ({
+  currentUserId,
+  userId,
+}: {
+  currentUserId: string;
+  userId: string;
+}) => {
+  return await prisma.user.update({
+    where: { id: currentUserId },
+    data: {
+      receivedRequests: { disconnect: { id: userId } },
+      friends: { connect: { id: userId } },
+    },
   });
 };
