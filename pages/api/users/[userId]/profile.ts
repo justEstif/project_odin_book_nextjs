114aa9f8647fde2b0c32775c28a596ba6bca1d2a
Estifanos <estifanosbeyene21@gmail.com>
2022-12-23 14:01:00 -0500

    feat: add profile update api request

--- pages/api/users/[userId]/profile.ts
+++ pages/api/users/[userId]/profile.ts
@@ -1,10 +1,18 @@
+import type { NextApiHandler } from "next";
 import prisma from "@/lib-server/prisma";
 import withAuth from "@/lib-server/middleware/with-auth";
-import type { NextApiHandler } from "next";
+import withValidation from "@/lib-server/middleware/with-validation";
+import {
+  profileSchema,
+  TProfileSchema,
+} from "@/lib-server/validations/profile";
 
-const handler: NextApiHandler<TGetResponse> = async (req, res) => {
+const handler: NextApiHandler<TGetResponse | TPostResponse> = async (
+  req,
+  res
+) => {
   const {
-    query: { userId },
+    query: { userId, currentUserId },
     method,
   } = req;
 
@@ -20,19 +28,56 @@ const handler: NextApiHandler<TGetResponse> = async (req, res) => {
       }
       res.status(403).end();
       break;
-
+    /**
+     * @description update profile
+     * @access any logged in user
+     */
+    case "POST":
+      if (
+        typeof userId === "string" &&
+        typeof currentUserId === "string" &&
+        userId === currentUserId
+      ) {
+        const profileBody = req.body as TProfileSchema;
+        const data = await updateProfile({ currentUserId, profileBody });
+        res.status(200).json(data);
+      }
+      res.status(403).end();
+      break;
     default:
-      res.setHeader("Allow", ["GET"]);
+      res.setHeader("Allow", ["GET", "POST"]);
       res.status(405).end(`Method ${method} Not Allowed`);
   }
 };
 
-export default withAuth(handler);
+export default withValidation(profileSchema, withAuth(handler));
 
 export type TGetResponse = Awaited<ReturnType<typeof getProfile>>;
 const getProfile = async (id: string) => {
   return await prisma.user.findUnique({
     where: { id },
     select: { profile: true },
+  });
+};
+
+export type TPostResponse = Awaited<ReturnType<typeof updateProfile>>;
+const updateProfile = async ({
+  currentUserId,
+  profileBody,
+}: {
+  currentUserId: string;
+  profileBody: TProfileSchema;
+}) => {
+  return await prisma.user.update({
+    where: { id: currentUserId },
+    data: {
+      profile: {
+        update: {
+          ...(profileBody.bio && { bio: profileBody.bio }),
+          ...(profileBody.image && { image: profileBody.image }),
+          ...(profileBody.name && { name: profileBody.name }),
+        },
+      },
+    },
   });
 };
