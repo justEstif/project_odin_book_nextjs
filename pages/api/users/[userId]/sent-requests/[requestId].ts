561d81c8cdfa6876e8edd3b879ab1b2bd85e257d
Estifanos <estifanosbeyene21@gmail.com>
2022-12-24 22:31:27 -0500

    feat: add sent request post and delete req handler

--- pages/api/users/[userId]/sent-requests/[requestId].ts
+++ pages/api/users/[userId]/sent-requests/[requestId].ts
@@ -1,5 +1,78 @@
 import { NextApiHandler } from "next";
+import prisma from "@/lib-server/prisma";
+import withAuth from "@/lib-server/middleware/with-auth";
 
-const handler: NextApiHandler = async (req, res) => { };
+const handler: NextApiHandler<TPostResponse> = async (req, res) => {
+  const {
+    method,
+    query: { userId, currentUserId, requestId },
+  } = req;
 
-export default handler;
+  switch (method) {
+    /**
+     * @description send a friend request
+     * @access any logged in user
+     */
+    case "POST":
+      if (
+        typeof userId === "string" &&
+        typeof currentUserId === "string" &&
+        typeof requestId === "string" &&
+        userId === currentUserId
+      ) {
+        const data = await sendFriendRequest({ currentUserId, requestId });
+        res.status(201).json(data);
+      }
+      res.status(403).end();
+      break;
+    /**
+     * @description delete a sent friend request
+     * @access any logged in user
+     */
+    case "DELETE":
+      if (
+        typeof userId === "string" &&
+        typeof currentUserId === "string" &&
+        typeof requestId === "string" &&
+        userId === currentUserId
+      ) {
+        const data = await deleteSentRequest({ currentUserId, requestId });
+        res.status(200).json(data);
+      }
+      res.status(403).end();
+      break;
+    default:
+      res.setHeader("Allow", ["POST", "DELETE"]);
+      res.status(405).end(`Method ${method} Not Allowed`);
+  }
+};
+
+export default withAuth(handler);
+
+export type TPostResponse = Awaited<ReturnType<typeof sendFriendRequest>>;
+const sendFriendRequest = async ({
+  currentUserId,
+  requestId,
+}: {
+  currentUserId: string;
+  requestId: string;
+}) => {
+  return await prisma.user.update({
+    where: { id: currentUserId },
+    data: { sentRequests: { connect: { id: requestId } } },
+  });
+};
+
+export type TDeleteResponse = Awaited<ReturnType<typeof deleteSentRequest>>;
+const deleteSentRequest = async ({
+  currentUserId,
+  requestId,
+}: {
+  currentUserId: string;
+  requestId: string;
+}) => {
+  return await prisma.user.update({
+    where: { id: currentUserId },
+    data: { receivedRequests: { disconnect: { id: requestId } } },
+  });
+};
