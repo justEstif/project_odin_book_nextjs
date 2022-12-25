4807cb4be76220cddacb91e2a396544dc009aab8
Estifanos <estifanosbeyene21@gmail.com>
2022-12-24 22:33:11 -0500

    feat: add delete friend request handler

---
+++ pages/api/users/[userId]/friends/[friendId].ts
@@ -0,0 +1,51 @@
+import { NextApiHandler } from "next";
+import prisma from "@/lib-server/prisma";
+import withAuth from "@/lib-server/middleware/with-auth";
+
+const handler: NextApiHandler<TDeleteResponse> = async (req, res) => {
+  const {
+    method,
+    query: { userId, currentUserId, requestId },
+  } = req;
+
+  switch (method) {
+    /**
+     * @description send a friend request
+     * @access any logged in user
+     */
+    case "DELETE":
+      if (
+        typeof userId === "string" &&
+        typeof currentUserId === "string" &&
+        typeof requestId === "string" &&
+        userId === currentUserId
+      ) {
+        const data = await deleteFriend({ currentUserId, requestId });
+        res.status(201).json(data);
+      }
+      res.status(403).end();
+      break;
+    default:
+      res.setHeader("Allow", ["DELETE"]);
+      res.status(405).end(`Method ${method} Not Allowed`);
+  }
+};
+
+export default withAuth(handler);
+
+export type TDeleteResponse = Awaited<ReturnType<typeof deleteFriend>>;
+const deleteFriend = async ({
+  currentUserId,
+  requestId,
+}: {
+  currentUserId: string;
+  requestId: string;
+}) => {
+  return await prisma.user.update({
+    where: { id: currentUserId },
+    data: {
+      friends: { disconnect: { id: requestId } },
+      friendsOf: { disconnect: { id: requestId } },
+    },
+  });
+};
