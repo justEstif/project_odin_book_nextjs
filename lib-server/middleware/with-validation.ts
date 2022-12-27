c3dc23e7eb54d2b1536edba9806f4c2fd256e621
Estifanos <estifanosbeyene21@gmail.com>
2022-12-26 20:57:49 -0500

    feat: add alternative validation middleware

--- lib-server/middleware/with-validation.ts
+++ lib-server/middleware/with-validation.ts
@@ -1,11 +1,43 @@
-import type { NextApiHandler } from "next";
+import type { NextApiHandler, NextApiRequest } from "next";
 import { z } from "zod";
 import type { ZodSchema } from "zod";
 
-const withValidation = <T extends ZodSchema>(
-  schema: T,
-  handler: NextApiHandler
-) => {
+type TValidation = {
+  requestMethod: string;
+  schema: ZodSchema;
+  handler: NextApiHandler;
+  validationTarget: keyof NextApiRequest;
+};
+
+// method, schema, obj
+const validation = ({
+  requestMethod,
+  schema,
+  handler,
+  validationTarget,
+}: TValidation) => {
+  const middleware: NextApiHandler = async (req, res) => {
+    switch (req.method) {
+      case requestMethod:
+        try {
+          await schema.parse(
+            req[validationTarget] ? req[validationTarget] : {}
+          );
+          return handler(req, res);
+        } catch (error) {
+          if (error instanceof z.ZodError) {
+            return res.status(422).json(error.issues);
+          }
+          return res.status(422).end();
+        }
+      default:
+        return handler(req, res);
+    }
+  };
+  return middleware;
+};
+
+const withValidation = (schema: ZodSchema, handler: NextApiHandler) => {
   const middleware: NextApiHandler = async (req, res) => {
     const { method } = req;
 
