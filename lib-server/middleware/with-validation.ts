0a2ad3f43bb49b76edd8df2be50ba6ed441d42d8
Estifanos <estifanosbeyene21@gmail.com>
2022-12-27 08:57:09 -0500

    feat: add different validation middleware

--- lib-server/middleware/with-validation.ts
+++ lib-server/middleware/with-validation.ts
@@ -8,6 +8,29 @@ type TValidation = {
   validationTarget: keyof NextApiRequest;
 };
 
+const withValidation2 = (
+  validations: TValidation[],
+  handler: NextApiHandler
+) => {
+  const middleware: NextApiHandler = (req, res) => {
+    for (const validation of validations) {
+      const { validationTarget, requestMethod, schema } = validation;
+      if (req.method === requestMethod) {
+        try {
+          schema.parse(req[validationTarget] ? req[validationTarget] : {});
+        } catch (error) {
+          if (error instanceof z.ZodError) {
+            return res.status(422).json(error.issues);
+          }
+          return res.status(422).end();
+        }
+      }
+    }
+    return handler(req, res);
+  };
+  return middleware;
+};
+
 const withValidation = (
   { validationTarget, requestMethod, schema }: TValidation,
   handler: NextApiHandler
